7b2584a2aa010189008a59e0a81f97268def0a06
nylenx <52587752+nylenx@users.noreply.github.com>
2023-09-23 23:13:04 +0530

    217. Contains Duplicate

---
+++ 217. Contains Duplicate/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+      
+        Set<Integer> uniqueSet = new HashSet<>();
+        
+        for(int i=0;i<nums.length;i++){
+            if(uniqueSet.contains(nums[i])){
+                return true;
+            }
+            else{
+                uniqueSet.add(nums[i]);
+            }
+        }
+        return false;
+    }
+}
